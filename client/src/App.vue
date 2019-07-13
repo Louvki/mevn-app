8f57cfbfce3f52796161393a0d7b3062ca9e7814
louvki <lukas.vismn@gmail.com>
2019-07-14 01:38:20 +0200

    switched to vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <div id="app">
-    <NavBar />
-    <router-view :key="$route.fullPath" />
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import CompanyList from "./views/CompanyList.vue";
-import NavBar from "./components/NavBar.vue";
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: "app",
+  name: 'App',
   components: {
-    CompanyList,
-    NavBar
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
-};
-</script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,9 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
-import router from './router';
-import store from './store/store';
 
-// const requireComponent = require.context(
-//   './components',
-//   false,
-//   /Base[A-Z]\w+\.(vue|js)$/
-// );
+Vue.config.productionTip = false
 
-// requireComponent.keys().forEach((fileName) => {
-//   const componentConfig = requireComponent(fileName);
-
-//   const componentName = upperFirst(
-//     camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
-//   );
-
-//   Vue.component(componentName, componentConfig.default || componentConfig);
-// });
-
-Vue.config.productionTip = false;
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App),
+}).$mount('#app')
